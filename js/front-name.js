daf3feeac38370e4f121f4c31c70c02a18c76926
Omkar <omkar108591@gmail.com>
2017-08-17 00:26:41 +0530

    UI improved.

--- js/front-name.js
+++ js/front-name.js
@@ -1,42 +1,66 @@
-// shim layer with setTimeout fallback
-    window.requestAnimFrame = (function(){
-      return  function( callback ){
-                window.setTimeout(callback, 1000);
-              };
-    })();
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#top-image").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#top-image').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+        $('.boxmove').backgroundMove({
+  movementStrength:'50'
+        });
+});
+     consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
 
-$(document).ready(function(){
-   
-  var $shadow = $('#shadow');
-  var shadowLimit = 200;
-  var moveEvent = ('ontouchstart' in document.documentElement) ? "touchmove" : "mousemove";
-  
-  (function animloop(){
-    requestAnimFrame(animloop);
-      
-    $(window).bind(moveEvent, function(ev){
-      var $this = $(this);
-      var w      = $this.width();
-      var h      = $this.height();
-      var center = { x: w/2, y: h/2 };
-    
-      var evX = (moveEvent == 'touchmove') ? ev.originalEvent.touches[0].clientX : ev.clientX;
-      var evY = (moveEvent == 'touchmove') ? ev.originalEvent.touches[0].clientY : ev.clientY;
-      
-      var shadowX = (center.x - evX) / 10;
-      var shadowY = (center.y - evY) / 10;
-      
-      shadowX = (shadowX > shadowLimit) ? shadowLimit : shadowX;
-      shadowX = (shadowX < shadowLimit*-1) ? shadowLimit*-1 : shadowX;
-      shadowY = (shadowY > shadowLimit) ? shadowLimit : shadowY;
-      shadowY = (shadowY < shadowLimit*-1) ? shadowLimit*-1 : shadowY;
-      
-      $shadow.css({ textShadow: Math.ceil(shadowX) + 'px '+ Math.ceil(shadowY) +'px '+ Math.abs(shadowX*shadowY)/100 +'px rgba(0,0,0,0.4)' });
-      
-      
-   });
-  })();
-   
-  
-}); 
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
 
